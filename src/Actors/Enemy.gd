a9b5e02eda36b9f97f0e44d81b6b97099d6ed3c1
VatrixtheMatrix <60194317+VatrixtheMatrix@users.noreply.github.com>
2020-03-10 22:14:20 -0400

    Phillip's E08b Debugging Assignment

--- src/Actors/Enemy.gd
+++ src/Actors/Enemy.gd
@@ -11,10 +11,12 @@ func _ready() -> void:
 	_velocity.x = -speed.x
 
 
+# warning-ignore:unused_argument
 func _physics_process(delta: float) -> void:
-	_velocity.x *= -1 if is_on_wall() else 1
+	_velocity.y += gravity * delta
+	if is_on_wall():
+		_velocity.x *= -1.0
 	_velocity.y = move_and_slide(_velocity, FLOOR_NORMAL).y
-	_velocity.y = -speed.x
 
 
 func _on_StompArea2D_area_entered(area: Area2D) -> void:
